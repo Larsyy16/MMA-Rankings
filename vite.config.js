762638736a3742a1bc8f2c49d7ea5ce544c611cf
Lars <lars.kolankiewicz@gmail.com>
2023-09-08 14:42:07 -0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://larsyy16.github.io/MMA-Rankings/",
+  base: "/MMA-Rankings",
   plugins: [react()],
   
 });
