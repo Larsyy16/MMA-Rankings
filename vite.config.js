614926b5d4fd95b9b8259e2114f4277589d3eb73
Lars <lars.kolankiewicz@gmail.com>
2023-09-11 12:02:20 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { createServer } from "miragejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MMA-Rankings/",
   plugins: [react()],
-  server: {
-    middleware: [createServer({ routes() {} }).middleware],
-  },
+
+  // base: "/MMA-Rankings/",
+  
 });
