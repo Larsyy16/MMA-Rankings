acd8921431b344666100d11a46302326560fb4a0
Lars <lars.kolankiewicz@gmail.com>
2023-09-11 12:16:39 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: "/MMA-Rankings/",
-  
+  base: process.env.NODE_ENV === 'production' ? "/MMA-Rankings/" : "/"
 });
+
