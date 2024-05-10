c51e6e93e36c16c7f86212ba1451232717a865df
Lars <lars.kolankiewicz@gmail.com>
2024-05-09 21:54:02 -0400

    change footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <footer> 
-            <p> <i>Made with love by Larsyy</i></p>
+            <p> <i>Made with love by Larsyy16</i></p>
 
 
         </footer>
